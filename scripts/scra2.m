6e6e405760097546210399a33f318e1a38ad2338
djk2120 <djk2120@columbia.edu>
2018-04-09 19:38:31 -0400

    more ms

--- scripts/scra2.m
+++ scripts/scra2.m
@@ -1,16 +1,13 @@
 clear
 close all
 
-n = 999999;
-a=randn(n,1);
+f='../data/k4g7.nc';
 
-kk = 50000+a*10000;
+pftname = ncread(f,'pftname');
+pftname(:,5)'
 
-p10 = 37184;
+s = ncread(f,'smpso');
+s(5)/101972
 
-revs = zeros(n,1);
-ix   = kk>p10;
-revs(ix)  = p10*0.2+(kk(ix)-p10)*0.05;
-revs(~ix) = kk(~ix)*0.2;
-
-mean(revs)
+s = ncread(f,'smpsc');
+s(5)/101972
