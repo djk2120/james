1189d927a0362c33cb5fa0c12bcd1dd849d8bac1
djk2120 <djk2120@columbia.edu>
2018-04-09 09:45:58 -0400

    move btran

--- scripts/scra2.m
+++ scripts/scra2.m
@@ -1,33 +1,16 @@
 clear
 close all
 
+n = 999999;
+a=randn(n,1);
 
-f = '../data/k4g7.nc';
-f = '../data/clm5_params.c171117.nc';
+kk = 50000+a*10000;
 
-aa= ncinfo(f);
+p10 = 37184;
 
-pft = ncread(f,'pftname')';
+revs = zeros(n,1);
+ix   = kk>p10;
+revs(ix)  = p10*0.2+(kk(ix)-p10)*0.05;
+revs(~ix) = kk(~ix)*0.2;
 
-
-kmax = ncread(f,'kmax');
-kmax(5,:)
-kmax(7,:)
-
-krmax = ncread(f,'krmax');
-krmax(5,:)
-krmax(7,:)
-
-p50 = ncread(f,'psi50');
-p50(5,:)
-p50(7,:)
-
-
-g1 = ncread(f,'medlynslope');
-g1(5,:)
-g1(7,:)
-
-
-g0 = ncread(f,'medlynintercept');
-g0(5,:)
-g0(7,:)
+mean(revs)
